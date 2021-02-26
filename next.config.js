62846729026ee40f770701d804de24248fd93aea
jinho <gogod23@ajou.ac.kr>
2021-02-26 12:33:05 +0900

    빌드전 체크 #2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
-// module.exports = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: "/api/:path*",
-//         destination: "http://localhost:4000/api/:path*",
-//       },
-//     ];
-//   },
-// };
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
+  compress: true,
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === "production";
+    return {
+      ...config,
+      mode: prod ? "production" : "development",
+      devtool: prod ? "hidden-source-map" : "eval",
+      plugins: [
+        ...config.plugins,
+        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+      ],
+    };
+  },
+});
