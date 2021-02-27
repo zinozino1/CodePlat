063742434c2a171009c01e40c336d1de52b3fe53
jinho <gogod23@ajou.ac.kr>
2021-02-27 14:47:36 +0900

    커스텀 훅 코드 정리

--- hooks/useInput.js
+++ hooks/useInput.js
@@ -1,5 +1,11 @@
 import React, { useState, useCallback } from "react";
 
+/**
+ * @author 박진호
+ * @version 1.0
+ * @summary input 커스텀 훅
+ */
+
 const useInput = (initialValue = "") => {
   const [value, setValue] = useState(initialValue);
   const handler = useCallback((e) => {
--- hooks/useToggle.js
+++ hooks/useToggle.js
@@ -1,5 +1,11 @@
 import React, { useState, useCallback } from "react";
 
+/**
+ * @author 박진호
+ * @version 1.0
+ * @summary toggle 커스텀 훅
+ */
+
 const useToggle = (initialValue = false) => {
   const [toggle, setToggle] = useState(initialValue);
   const handler = useCallback(() => {
